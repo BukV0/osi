afa1760c0a4b28b33df6fa8e529833203c85f54e
BukV0 <32202179+BukV0@users.noreply.github.com>
2022-01-25 17:06:40 +0300

    robit argv

--- prog.c
+++ prog.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<stdlib.h>
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
@@ -8,7 +8,7 @@
 #define NOT_FOUND_ERROR 2
 #define CANT_OPEN_ERROR 3
 
-
+char* arr;
 int interrupt_counter = 0;
 void interrupt_handler()       // handler of keyboard interrupt signal
 {
@@ -27,17 +27,87 @@ void get_correct_file_name(char* filename){
         {
             printf("\nFile \"%s\" not found\n", filename);
         } else {
+            //printf("okey");
             return;
         }
+        //printf("okettty");
     }
 }
 
 
-int main(int argc, char** argv){
+void add_into_array(char *str,int fns){
+    if(fns == 1){
+        arr = (char *)malloc(256);
+        strcpy(arr,str);
+        return;
+    }
+    else{ 
+        arr = (char *)realloc(arr,256*fns);
+        strcat(arr,str);
+        return;
+    }
+}
+
+
+int Search_in_File(char *fname, char *str) {
+	FILE *fp = fopen(fname, "r");
+	int line_num = 1;
+	int find_result = 0;
+	char temp[256];
+	while(fgets(temp, 256, fp) != NULL) {
+		if((strstr(temp, str)) != NULL) {
+			find_result++;
+            add_into_array(temp,find_result);
+		}
+		line_num++;
+	}
+
+	if(find_result == 0) {
+		printf("\nSorry, couldn't find a match.\n");
+	}
+	
+	if(fp) {
+		fclose(fp);
+	}
+   	return(0);
+}
+
+int main(){
         // rewrite the KeyboardInterrupt signal handler
    signal(SIGINT,interrupt_handler);
-    char filename[256];                                // file name parameter
 
+    char filename[256]; 
+    // file name parameter
+    char template[256];
+    while(1){
+    template[0] = 'f';
+    template[1] = ' ';
     get_correct_file_name(&filename);
+    int k = Search_in_File(filename,template);
+    printf("a.c pid %d\n",getpid());
+    char *argg[] = {arr,NULL};
+    execv("./tes",argg);
+    }
+   
 
 }
+
+/*
+memset
+scanf
+printf
+strstr
+malloc
+realloc
+execv
+signal
+SIGINT
+fopen
+fclose
+strcpy
+sctrcat
+access
+exit
+fprinf
+stderr
+*/
